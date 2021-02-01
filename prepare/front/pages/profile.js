aa87ca1d11f3f319cf0cdd7bafc08551d96b1b50
최현우 <escape0619@gmail.com>
2021-02-01 09:59:10 +0900

    error fix

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -8,7 +8,7 @@ const Profile = () => {
     return (
         <>  
             <Head>
-                <meta charset="utf-8"/>
+                <meta charSet="utf-8"/>
                 <title>내 프로필 | NodeBird</title>
             </Head>  
             <AppLayout>내 프로필</AppLayout>
--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -7,7 +7,7 @@ const Signup = () => {
     return (
         <>  
             <Head>
-                <meta charset="utf-8"/>
+                <meta charSet="utf-8"/>
                 <title>회원가입 | NodeBird</title>
             </Head>  
             <AppLayout>회원가입 페이지</AppLayout>
