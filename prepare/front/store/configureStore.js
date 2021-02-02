cebcda06a477a499c6be1a1608c9f1ac79a42463
최현우 <escape0619@gmail.com>
2021-02-02 17:01:13 +0900

    redux 추가

---
+++ prepare/front/store/configureStore.js
@@ -0,0 +1,13 @@
+import { createWrapper }  from 'next-redux-wrapper';
+import { createStore } from 'redux';
+
+
+const configureStore = () => {
+    const store = createStore(reducer)
+};
+
+const wrapper = createWrapper(configureStore, {
+    debug: ProcessingInstruction.env.NODE_ENV === 'development',
+});
+
+export default wrapper;
