506f1e7022ffc54e89339f702bb7316de90b5397
최현우 <escape0619@gmail.com>
2021-02-16 09:54:19 +0900

    리듀서에 사가 적용

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -7,18 +7,56 @@ const dummyUser = {
   };
   
   export const initialState = {
-    isLoggedIn: false,
+    followLoading: false, // 팔로우 시도중
+    followDone: false,
+    followError: null,
+    unfollowLoading: false, // 언팔로우 시도중
+    unfollowDone: false,
+    unfollowError: null,
+    logInLoading: false, // 로그인 시도중
+    logInDone: false,
+    logInError: null,
+    logOutLoading: false, // 로그아웃 시도중
+    logOutDone: false,
+    logOutError: null,
+    signUpLoading: false, // 회원가입 시도중
+    signUpDone: false,
+    signUpError: null,
+    changeNicknameLoading: false, // 닉네임 변경 시도중
+    changeNicknameDone: false,
+    changeNicknameError: null,
     me: null,
     signUpData: {},
     loginData: {},
   };
   
-  export const SIGN_UP = 'SIGN_UP';
+  export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+  export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+  export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+  
+  export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+  export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+  export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+  
+  export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
   export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
-  export const LOG_IN = 'LOG_IN'; // 액션의 이름
-  export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'; // 액션의 이름
-  export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'; // 액션의 이름
-  export const LOG_OUT = 'LOG_OUT';
+  export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+  
+  export const CHANGE_NICKNAME_REQUEST = 'CHANGE_NICKNAME_REQUEST';
+  export const CHANGE_NICKNAME_SUCCESS = 'CHANGE_NICKNAME_SUCCESS';
+  export const CHANGE_NICKNAME_FAILURE = 'CHANGE_NICKNAME_FAILURE';
+  
+  export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
+  export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
+  export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
+  
+  export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
+  export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
+  export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
+  
+  export const ADD_POST_TO_ME = 'ADD_POST_TO_ME';
+  export const REMOVE_POST_OF_ME = 'REMOVE_POST_OF_ME';
+  
   
   export const signUpAction = (data) => {
     return {
@@ -49,7 +87,7 @@ const dummyUser = {
   
   export default (state = initialState, action) => {
     switch (action.type) {
-      case LOG_IN: {
+      case LOG_IN_REQUEST: {
         return {
           ...state,
           isLoggedIn: true,
@@ -57,7 +95,23 @@ const dummyUser = {
           loginData: action.data,
         };
       }
-      case LOG_OUT: {
+      case LOG_IN_SUCCESS: {
+        return {
+          ...state,
+          isLoggedIn: true,
+          me: dummyUser,
+          loginData: action.data,
+        };
+      }
+      case LOG_IN_FAILURE: {
+        return {
+          ...state,
+          isLoggedIn: true,
+          me: dummyUser,
+          loginData: action.data,
+        };
+      }
+      case LOG_OUT_REQUEST: {
         return {
           ...state,
           isLoggedIn: false,
