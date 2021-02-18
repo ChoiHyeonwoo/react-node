702c26d4e0e8b019fe69687c2f32d20786f47736
최현우 <escape0619@gmail.com>
2021-02-18 09:19:56 +0900

    express setting

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,8 +1,15 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    res.end('Hello node');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Express');
 });
-server.listen(3065, () => {
+
+app.get('/api', (req, res) => {
+    res.send('Hello API');
+});
+
+app.listen(3065, () => {
     console.log('서버 실행 중');
 });
