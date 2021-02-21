b054d932cdf6ee651620d562214a569e90a13a81
HyeonWooChoi <hwchoi@deepnoid.com>
2021-02-22 08:48:05 +0900

    route 분리

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const postRouter = require('./routes/post');
+
 
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.send('Hello Express');
 });
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello API');
 });
+
+app.use('post', postRouter);
 
 app.listen(3065, () => {
     console.log('서버 실행 중');
